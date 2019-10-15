3b2973b7eb6f98cea85175ab231c3e36bb54cf0c
niulongjie@cmcm.com <niulongjie@conew.com>
2019-10-15 15:46:53 +0800

    上传

---
+++ Script/WoShiShui.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoShiShui : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("WoShiShui........Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("WoShiShui........Update");
+    }
+}
